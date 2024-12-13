46eefad68ed4bfaaf5ea42ee5c6f251b4734c810
LuizaR0drigues <132076247+LuizaR0drigues@users.noreply.github.com>
2024-12-13 15:56:36 -0300

    Update quiz.cpp

--- quiz.cpp
+++ quiz.cpp
@@ -1,55 +1,61 @@
 #include "quiz.h"
-//construtor
+// CLASSE JOGADOR E SUAS FUNÇÔES
+// construtor
 Jogador::Jogador(const std::string aux_id = "", const std::string aux_senha = "")
-   {
-        _id = aux_id;
-        _senha =  aux_senha;
-        _scoregeral = 0;
-        _scores = {};
-   };
+{
+    _id = aux_id;
+    _senha = aux_senha;
+    _scoregeral = 0;
+    _scores = {};
+};
 
-//Metodos
-    //Verifica se as credenciais do jogador estao corretas
-    bool Jogador::identificar_player(std::string nome, std::string senha){
-        if(_id.compare(nome) == 0  && _senha.compare(senha) == 0)
-            return true;
-        else 
-            return false;
-    };
+// Metodos
+// Verifica se as credenciais do jogador estao corretas
+bool Jogador::identificar_player(std::string nome, std::string senha)
+{
+    if (_id.compare(nome) == 0 && _senha.compare(senha) == 0)
+        return true;
+    else
+        return false;
+};
 
-    
-    //operadores
-    //operador responsavel por compara se a pont_playerA > pont_playerB
-    bool operator>(const Jogador& player_A, const Jogador& player_B) {
-        return player_A._scoregeral > player_B._scoregeral;
-    }
+// operadores
+// operador responsavel por compara se a pont_playerA > pont_playerB
+bool operator>(const Jogador &player_A, const Jogador &player_B)
+{
+    return player_A._scoregeral > player_B._scoregeral;
+}
 
-    bool operator<(const Jogador& player_A, const Jogador& player_B) {
-        return player_A._scoregeral < player_B._scoregeral;
-    }
+// operador responsavel por compara se a pont_playerA < pont_playerB
+bool operator<(const Jogador &player_A, const Jogador &player_B)
+{
+    return player_A._scoregeral < player_B._scoregeral;
+}
+// operador responsavel por compara se a pont_playerA == pont_playerB
+bool operator==(const Jogador &player_A, const Jogador &player_B)
+{
+    return player_A._scoregeral == player_B._scoregeral;
+}
 
-    bool operator==(const Jogador& player_A, const Jogador& player_B) {
-        return player_A._scoregeral == player_B._scoregeral;
-    }
+// getters
+int Jogador::get_scoregeral() const
+{
+    return _scoregeral;
+};
 
-    //getters
-    
-    int Jogador:: get_scoregeral() const {
-        return _scoregeral;
-    };
+std::string Jogador::get_Id() const
+{
+    return _id;
+};
 
-    std::string Jogador::get_Id() const{
-        return _id;
-    };
+// setters
+void Jogador::set_scoregeral(const int pontuacao)
+{
+    _scoregeral = pontuacao;
+}
+void Jogador::set_id(const std::string id)
+{
+    _id = id;
+}
 
-    //setters
-    void Jogador::set_scoregeral(const int pontuacao)
-    {
-        _scoregeral = pontuacao;
-    }
-    void Jogador::set_id(const std::string id)
-    {
-        _id = id;
-    }
-
-  
+// CLASSE PERGUNTAS
